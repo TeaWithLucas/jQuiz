b9b1782c73b24c263ff005279ad48bdb3a630a25
Kyle Davies <acc.kgd@outlook.com>
2018-04-20 14:24:16 +0100

    First commit

---
+++ jQuiz/QuizModel.java
@@ -0,0 +1,20 @@
+package jQuiz;
+
+/**
+ * @author  C1740129
+ * @author  Kyle Morris
+ * @version 1.0
+ * @since   2018-04-04
+ */
+public class QuizModel {
+    
+    /**
+     * main()
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Code here...
+    }
+
+
+}
