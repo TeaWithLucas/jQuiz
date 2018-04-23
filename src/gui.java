60209e3653d3bdb36bb397a67a16d8194f7d0bcc
Lucas <18538046+TeaWithLucas@users.noreply.github.com>
2018-04-23 16:33:44 +0100

    Updated GUI

--- src/gui.java
+++ src/gui.java
@@ -25,6 +25,8 @@ public class gui extends Application {
     private static final String styleShow = "-fx-skin: \"com.sun.javafx.scene.control.skin.ButtonSkin\"; -fx-base: gray; -fx-padding: 2; -fx-font-weight: bold; -fx-text-fill: #FFF; -fx-background-radius: 10 0 0 10;";
     private final double minWidth = 5;
     private final double maxWidth = 170;
+    private final int Width = 1080;
+    private final int Height = 980;
     private boolean hidden = true;
 
     public static void main(String[] args) {
@@ -32,20 +34,22 @@ public class gui extends Application {
     }
 
     private Pane root;
-
+    private Stage stage;
+    
     @Override
     public void start(Stage primaryStage) {
+    	stage = primaryStage;
     	primaryStage.setTitle("jQuiz Master");
+    	startScreen();
     	
-    	QuizStartScreen(primaryStage);
     	primaryStage.show();
     }
     
-    private void Question(Stage stage) {
+    private void QuestionFrame() {
     	BorderPane borderPane = new BorderPane();
         root = new Pane();
         root.getChildren().add(borderPane);
-    	Scene questionScene = new Scene(root, 600, 500);
+    	Scene questionScene = new Scene(root, Width, Height);
     	questionScene.getStylesheets().add("question.css");
     	
         
@@ -53,69 +57,84 @@ public class gui extends Application {
         borderPane.prefWidthProperty().bind(root.widthProperty());
         borderPane.prefHeightProperty().bind(root.heightProperty());
 
-        final Pane rightPane = getRightContent();
-        borderPane.setRight(rightPane);
-        
         String question = "Question 1: Where's the beef?";
-        final Pane topPane = getTopContent(question);
-        borderPane.setTop(topPane);
+        Pane topPane = questionMenuTop();
+        borderPane.setTop(topPane);	
         
-        VBox box = new VBox();
-        box.setStyle("-fx-background-color: lightgray;");
-        box.setAlignment(Pos.CENTER_RIGHT);
+        GridPane middlePane = new GridPane();
+        borderPane.setCenter(middlePane);
         
         String[] answers = {"test1", "Test2"};
-        final Pane bottomPane = getBottomContent(answers);
-        borderPane.setBottom(bottomPane);
-        
-        
-        borderPane.setCenter(box);
-
-        final Button btn = new Button(" toggle button ");
-        btn.setPrefHeight(40);
-        btn.setFocusTraversable(false);
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            public void handle(ActionEvent event) {
-                toggleRightContent(btn, rightPane);
-            }
-        });
-
-        root.getChildren().add(btn);
-        toggleRightContent(btn, rightPane);
+        middlePane.addRow(0, questionFrame(question));
+        middlePane.addRow(1, answerFrame(answers));     
         
         stage.setScene(questionScene);
 
         
     }
     
-    private void QuizStartScreen(Stage stage) {
+    private void startScreen() {
     	BorderPane borderPane = new BorderPane();
         root = new Pane();
         root.getChildren().add(borderPane);
-    	Scene quizSSScene = new Scene(root, 600, 500);
-    	quizSSScene.getStylesheets().add("quizstartscreen.css");
+    	Scene startScene = new Scene(root, Width, Height);
+    	startScene.getStylesheets().add("startscreen.css");
         
         
 
         borderPane.prefWidthProperty().bind(root.widthProperty());
         borderPane.prefHeightProperty().bind(root.heightProperty());
         
+        VBox mainBox = new VBox();
+        mainBox.setStyle("-fx-background-color: lightgray;");
+        mainBox.setAlignment(Pos.CENTER);
+        
+        borderPane.setCenter(mainBox);
+        
+        Text mainText = new Text("jQuiz");
+        mainText.setFont(Font.font ("Verdana", 40));
+        mainBox.getChildren().add(mainText);
+        
+        VBox choiceBox = new VBox();
+        choiceBox.setStyle("-fx-background-color: lightgray;");
+        choiceBox.setAlignment(Pos.CENTER);
+        Button quizButton = new Button("Quiz");
+        quizButton.setOnMouseClicked(e -> { QuizStartScreen("Poo and shit"); });
+        choiceBox.getChildren().add(quizButton);
+        Button loginButton = new Button("Login");
+        loginButton.setOnMouseClicked(e -> { QuizStartScreen("Poo and shit"); });
+        choiceBox.getChildren().add(loginButton);
+        
+        borderPane.setCenter(choiceBox);
+        
+        stage.setScene(startScene);
+    }
+    
+    private void QuizStartScreen(String topicName) {
+    	BorderPane borderPane = new BorderPane();
+        root = new Pane();
+        root.getChildren().add(borderPane);
+    	Scene quizSSScene = new Scene(root, Width, Height);
+    	quizSSScene.getStylesheets().add("quizstartscreen.css");
+    	
+        borderPane.prefWidthProperty().bind(root.widthProperty());
+        borderPane.prefHeightProperty().bind(root.heightProperty());
+        
+        Pane topPane = questionMenuTop();
+        borderPane.setTop(topPane);	
+        
+        
         VBox box = new VBox();
         box.setStyle("-fx-background-color: lightgray;");
         box.setAlignment(Pos.CENTER);
         
-        Text mainText = new Text("Welcome!!");
+        Text mainText = new Text("Welcome to the Quiz on "+ topicName);
         mainText.setFont(Font.font ("Verdana", 40));
         box.getChildren().add(mainText);
         Text getStarted = new Text("Click here to start!");
         getStarted.setFont(Font.font ("Verdana", 30));
         box.getChildren().add(getStarted);
-        getStarted.setOnMouseClicked(e -> {
-        	Question(stage);
-        });
-
-        
+        getStarted.setOnMouseClicked(e -> { QuestionFrame(); });
         
         borderPane.setCenter(box);
 
@@ -127,25 +146,43 @@ public class gui extends Application {
         GridPane pane = new GridPane();
 
         Text txt = new Text("Right Content");
-        txt.setWrappingWidth(maxWidth - 20);
+        //txt.setWrappingWidth(maxWidth - 20);
 
-        pane.setVgap(10);
+        //pane.setVgap(10);
         pane.setStyle("-fx-padding: 5; -fx-background-color: gray;");
         pane.addColumn(0, txt);
 
         return pane;
     }
     
-    private GridPane getTopContent(String question) {
+    private GridPane questionFrame(String question) {
         GridPane pane = new GridPane();
 
         Text txt = new Text(question);
-        txt.setWrappingWidth(maxWidth - 20);
+        txt.setFont(Font.font ("Verdana", 30));
+        //txt.setWrappingWidth(maxWidth - 20);
 
-        pane.setVgap(10);
-        pane.setStyle("-fx-padding: 5; -fx-background-color: gray;");
+        //pane.setVgap(10);
+        //pane.setStyle("-fx-padding: 5; -fx-background-color: gray;");
         pane.addColumn(0, txt);
+        
+        return pane;
+    }
+    
+    private GridPane questionMenuTop() {
+        GridPane pane = new GridPane();
 
+        Button quitButton = new Button("< Quit");
+        Button restartButton = new Button("Restart");
+        
+        quitButton.setOnMouseClicked(e -> { startScreen(); });
+        restartButton.setOnMouseClicked(e -> { QuizStartScreen("Poo and shit"); });
+
+        //pane.setVgap(10);
+        pane.setStyle("-fx-padding: 5; -fx-background-color: gray;");
+        pane.addColumn(0, quitButton);
+        pane.addColumn(1, restartButton);
+        
         return pane;
     }
     
@@ -154,7 +191,7 @@ public class gui extends Application {
         GridPane pane = new GridPane();
         
        
-        pane.setVgap(10);
+        //pane.setVgap(10);
         pane.setStyle("-fx-padding: 5; -fx-background-color: gray;");
         
         pane.addRow(0, new Label("Answers"));
@@ -164,21 +201,19 @@ public class gui extends Application {
 
         return pane;
     }
+    
+    private GridPane answerFrame(String[] Answers) {
+       GridPane pane = new GridPane();
+       
+       
+       //pane.setVgap(10);
+       pane.setStyle("-fx-padding: 5; -fx-background-color: gray;");
+       
+       pane.addRow(0, new Label("Answers"));
+       for(int i=0; i<Answers.length;i++) {
+    	   pane.add(new Button(Answers[i]), i, 1);
+       }
 
-    private void toggleRightContent(Button btn, Pane pane) {
-        if (hidden) {
-            btn.setStyle(styleHide);
-            pane.setMaxWidth(maxWidth);
-            pane.setMinWidth(maxWidth);
-            pane.setPrefWidth(maxWidth);
-            btn.layoutXProperty().bind(root.widthProperty().subtract(maxWidth));
-        } else {
-            btn.setStyle(styleShow);
-            pane.setMaxWidth(minWidth);
-            pane.setMinWidth(minWidth);
-            pane.setPrefWidth(minWidth);
-            btn.layoutXProperty().bind(root.widthProperty().subtract(minWidth).subtract(btn.widthProperty()));
-        }
-        hidden = !hidden;
+        return pane;
     }
 }
